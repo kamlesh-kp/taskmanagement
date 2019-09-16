9c344ea898d8d746207ec23f2a933f9f0e6d7eb6
kamlesh-kp <parmarkamleshk@gmail.copm>
2019-09-16 21:06:57 +0530

    modified

--- myfirstapi/settings.py
+++ myfirstapi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -73,9 +75,11 @@ WSGI_APPLICATION = 'myfirstapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#database
+
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 DATABASES = {
