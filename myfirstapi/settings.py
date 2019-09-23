113ca84d4b850b5f8b2137f58807073bb9927aa1
kamlesh-kp <parmarkamleshk@gmail.copm>
2019-09-23 21:31:06 +0530

    modified settting.py

--- myfirstapi/settings.py
+++ myfirstapi/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = 'myfirstapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#database
 
-# DATABASES = {'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'))
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
